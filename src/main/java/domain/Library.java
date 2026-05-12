35ffc9369f4fa9c02f9ba864b2c318f03821494a
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-05-12 11:32:59 -0400

    working on exception handling in Library.java

--- src/main/java/domain/Library.java
+++ src/main/java/domain/Library.java
@@ -23,4 +23,30 @@ public class Library {
     public void addUser(User user) {
         users.add(user);
     }
+
+    public void borrowItem(User user, Item item) {
+        try {
+            if (item.getStatus().equalsIgnoreCase("Borrowed")) ;
+            {
+                throw new Exception("Item already borrowed");
+            }
+
+            // IntelliJ says statement is unreachable, but I'm not sure why
+            if (user.getBorrowedItems().size() >= user.getBorrowLimit()) {
+                throw new Exception("Borrow limit reached");
+            }
+
+            item.setStatus("Borrowed");
+            user.getBorrowedItems().add(item);
+            System.out.println("Item borrowed successfully");
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void returnItem(User user, Item item) {
+        item.setStatus("In store");
+        user.getBorrowedItems().remove(item);
+    }
 }
