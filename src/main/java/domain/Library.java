7b0ac3cf6e3bfb3c5dad341f0e2919951b37990b
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-05-12 11:18:49 -0400

    correction: NOT done reportable, meant to push library.java. Reportable needs fixing

--- src/main/java/domain/Library.java
+++ src/main/java/domain/Library.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+    private List<Item> items;
+    private List<User> users;
+
+    public Library(List<Item> items, List<Item> users) {
+        this.items = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
 }
