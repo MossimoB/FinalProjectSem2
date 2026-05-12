19fd5b17b492515892b99f5418956f648081c9ec
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-05-12 11:03:26 -0400

    Forgot how to do String.format, will correct later

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -1,4 +1,17 @@
 package domain;
 
-public class Item {
+import lombok.*;
+
+@Getter
+@Setter
+public abstract class Item {
+    protected int id;
+    protected String title;
+    protected String status;
+
+    public Item(int id, String title, String status) {
+        this.id = String.format("%04d, ");
+        this.title = title;
+        this.status = status;
+    }
 }
