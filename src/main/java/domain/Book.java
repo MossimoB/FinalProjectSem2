f3393526b73efab6f47d7d74bfb144ba63500518
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-05-12 11:06:09 -0400

    Finished Book.java (I think)

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -1,4 +1,18 @@
 package domain;
 
-public class Book {
+import lombok.*;
+
+@Getter
+@Setter
+public class Book extends Item {
+    private String isbn;
+    private String author;
+    private String genre;
+
+    public Book(int id, String title, String status, String isbn, String author, String genre) {
+        super(id, title, status);
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
 }
