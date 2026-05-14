f98b21aa8e1de0d88fb2aa78b1301b9928639f4b
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-05-14 12:12:01 -0400

    updated the csv file and the main java file

--- src/main/java/org/mossimo/Main.java
+++ src/main/java/org/mossimo/Main.java
@@ -3,37 +3,40 @@ package org.mossimo;
 import domain.*;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Library library = new Library();
-        Student student = new Student(1, "Alice");
 
-        Book book = new Book(
-                1,
-                "Harry Potter1",
-                "in store",
-                "1234567890123",
-                "J.K. Rowling",
-                "Fantasy"
-        );
-
-        library.addUser(student);
-        library.addItem(book);
-        library.borrowItem(student, book);
-
-
-
-        library.loadBooksFromCSV("src/main/resources/books.csv");
+        library.loadItemsFromCSV("src/main/resources/books.csv");
         library.loadUsersFromCSV("src/main/resources/users.csv");
 
         library.sortItemsByTitle();
         library.sortUsersByName();
 
-        System.out.println("Books found by title:");
+        Student student = new Student(1, "Alice");
+
+        Book book = new Book(
+                10,
+                "Java Programming",
+                "In store",
+                "9999999999999",
+                "John Smith",
+                "Education"
+        );
+
+        library.borrowItem(student, book);
+        library.returnItem(student, book);
+
+        Admin admin = new Admin(99, "Admin");
+
+        admin.setLibrary(library);
+        admin.generateReport();
+
+        System.out.println("\nSearch by title:");
         System.out.println(library.streamSearch("Harry Potter"));
-        System.out.println("Books found by author:");
+
+        System.out.println("\nSearch by author:");
         System.out.println(library.searchByAuthor("J.K. Rowling"));
 
-        // backup csv files
         library.saveBooksToCSV("src/main/resources/books_backup.csv");
         library.saveUsersToCSV("src/main/resources/users_backup.csv");
     }
